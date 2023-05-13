7050d2aca2439d8d8702ee02a33efbd61cfa6ec2
Raj Rajhans <me@rajrajhans.com>
2023-05-13 18:10:25 +0530

    chore: refactor

--- lib/media_search_demo/clip/clip_index_agent.ex
+++ lib/media_search_demo/clip/clip_index_agent.ex
@@ -4,25 +4,11 @@ defmodule MediaSearchDemo.Clip.ClipIndexAgent do
   alias MediaSearchDemo.ANN
   alias MediaSearchDemo.Constants
 
-  @dimension 512
+  require Logger
 
   def start_link(opts) do
-    filenames_path =
-      opts
-      |> Keyword.get(
-        :filename_path,
-        Constants.default_filenames_save_path()
-      )
-
-    filenames = File.read!(filenames_path) |> Jason.decode!()
-
-    ann_index_path = opts |> Keyword.get(:ann_index_path, Constants.default_ann_index_save_path())
-
-    ann_index =
-      ANN.load_index(
-        ann_index_path,
-        Constants.clip_embedding_size()
-      )
+    filenames = init_filenames(opts)
+    ann_index = init_ann_index(opts)
 
     Agent.start_link(
       fn ->
@@ -41,5 +27,38 @@ defmodule MediaSearchDemo.Clip.ClipIndexAgent do
 
   def get_filenames() do
     Agent.get(__MODULE__, fn state -> state.filenames end)
+  end
+
+  def init_filenames(opts) do
+    try do
+      filenames_path =
+        opts
+        |> Keyword.get(
+          :filename_path,
+          Constants.default_filenames_save_path()
+        )
+
+      File.read!(filenames_path) |> Jason.decode!()
+    rescue
+      e ->
+        Logger.error("[CLIP_INDEX] Failed to load filenames: #{inspect(e)}")
+        []
+    end
+  end
+
+  def init_ann_index(opts) do
+    try do
+      ann_index_path =
+        opts |> Keyword.get(:ann_index_path, Constants.default_ann_index_save_path())
+
+      ANN.load_index(
+        ann_index_path,
+        Constants.clip_embedding_size()
+      )
+    rescue
+      e ->
+        Logger.error("[CLIP_INDEX] Failed to load index: #{inspect(e)}")
+        nil
+    end
   end
 end
