9a4c8848eb9e0a3122aeb49650a26240441321bf
Raj Rajhans <me@rajrajhans.com>
2023-05-25 00:16:58 +0530

    fix: broken test

--- test/media_search_demo_web/controllers/page_controller_test.exs
+++ test/media_search_demo_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MediaSearchDemoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Natural Language Media Search"
   end
 end
